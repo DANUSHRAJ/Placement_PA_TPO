9a6188b517ebae7e0b7d96e556af1a2614e7c431
DANUSHRAJ <mldanushraj2001@gmail.com>
2022-02-16 20:57:38 +0530

    Intership commit

--- lib/Screens/Intership.dart
+++ lib/Screens/Intership.dart
@@ -10,6 +10,34 @@ class Intership extends StatefulWidget {
   _IntershipState createState() => _IntershipState();
 }
 
+class NewObject {
+  String title;
+  IconData icon;
+
+  NewObject(this.title, this.icon);
+}
+
+List<NewObject> title = <NewObject>[
+  NewObject('Batch', Icons.description),
+  NewObject('Mr', Icons.person),
+  NewObject('Ms', Icons.person),
+];
+
+List<NewObject> department = <NewObject>[
+  NewObject('Dept', Icons.description),
+  NewObject('B.Tech IT', Icons.mobile_friendly_rounded),
+  NewObject('B.E CSE', Icons.computer_rounded),
+  NewObject('B.E ECE', Icons.signal_cellular_alt_outlined),
+  NewObject('B.E EEE', Icons.lightbulb_outline_rounded),
+  NewObject('B.E MECH', Icons.settings_applications_rounded),
+  NewObject('B.E CIVIL', Icons.apartment_outlined),
+];
+
+List<NewObject> dropbox = <NewObject>[
+  title.first, //0
+  department.first, //1
+];
+
 class _IntershipState extends State<Intership> {
   Widget _backButton() {
     return InkWell(
@@ -65,26 +93,82 @@ class _IntershipState extends State<Intership> {
             child: Align(
                 alignment: Alignment.topLeft,
                 child: Stack(children: <Widget>[
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height,
-                    width: MediaQuery.of(context).size.width,
-                    child: Divider(
-                        height: height * .5,
-                        thickness: 5,
-                        indent: 5,
-                        endIndent: 5,
-                        color: Colors.white),
-                  ),
                   SingleChildScrollView(
                       child: Column(children: [
                     SizedBox(height: height * .13),
                     CardFb1(text: "Explore", onPressed: () {}),
+                    Divider(
+                        //height: height * .,
+                        thickness: 5,
+                        indent: 5,
+                        endIndent: 5,
+                        color: Colors.white),
                   ]))
                 ])),
           )
         ])));
   }
 }
+
+Widget _DropBox(String title, List<NewObject> key, int i) {
+  return Container(
+    margin: EdgeInsets.symmetric(vertical: 10),
+    width: 250,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          title,
+          style: GoogleFonts.portLligatSans(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.limeAccent,
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          width: 180,
+          // padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: Colors.white,
+            //border: Border.all(color: Colors.deepOrange, width: 4),
+          ),
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton<NewObject>(
+              value: dropbox.elementAt(i), // currently selected item
+              items: key
+                  .map((item) => DropdownMenuItem<NewObject>(
+                        child: Row(
+                          children: [
+                            Icon(item.icon),
+                            const SizedBox(width: 8),
+                            Text(
+                              item.title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                        value: item,
+                      ))
+                  .toList(),
+              onChanged: (value) => setState(() {
+                dropbox[i] = value!;
+              }),
+            ),
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+setState(Null Function() param0) {}
 
 class CardFb1 extends StatelessWidget {
   final String text;
@@ -103,10 +187,10 @@ class CardFb1 extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Container(
           width: 400,
-          height: 250,
+          height: 300,
           padding: const EdgeInsets.all(15.0),
           decoration: BoxDecoration(
-            color: Colors.white,
+            color: Colors.black.withOpacity(0.2),
             borderRadius: BorderRadius.circular(12.5),
             boxShadow: [
               BoxShadow(
@@ -119,21 +203,47 @@ class CardFb1 extends StatelessWidget {
           child: Column(
             children: [
               //Image.network(imageUrl, height: 59, fit: BoxFit.cover),
-              const Spacer(),
-              Text(text,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  )),
-              const SizedBox(
-                height: 5,
+              Row(
+                children: [
+                  Column(
+                    children: [
+                      _DropBox("Batch", title, 0),
+                      _DropBox("Dept", department, 1),
+                    ],
+                  ),
+                  FloatingActionButton(onPressed: onPressed)
+                ],
               ),
 
-              const SizedBox(
-                height: 10,
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Write message...",
+                        hintStyle: TextStyle(color: Colors.white),
+                        //border: InputBorder.
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.send,
+                      color: Colors.white,
+                      size: 18,
+                    ),
+                    backgroundColor: Colors.orangeAccent,
+                    elevation: 0,
+                  ),
+                ],
               ),
+              // const SizedBox(
+              //       height: 5,
+              //     ),
             ],
           ),
         ),
