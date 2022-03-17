f773ae522068940e7f40d90a24c087b18799f7b1
DANUSHRAJ <mldanushraj2001@gmail.com>
2022-03-17 10:55:07 +0530

    Server commit

--- lib/API/forms.api.dart
+++ lib/API/forms.api.dart
@@ -6,7 +6,7 @@ import 'package:dio/dio.dart';
 class FormsApi {
 //  static String apiUrl = 'https://6081-27-57-63-29.ngrok.io'  'http://127.0.0.1:8081'  'https://sppdemo.herokuapp.com';
 
-  static String apiUrl = 'http://127.0.0.1:8080';
+  static String apiUrl = 'https://sjit-admin.herokuapp.com/';
 
   final _dio = Dio(BaseOptions(baseUrl: apiUrl));
 
