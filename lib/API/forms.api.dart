4a2c63b55c9b5bc747d5597ba28d6272de5a6a8f
DANUSHRAJ <mldanushraj2001@gmail.com>
2022-03-18 11:03:45 +0530

    Forms api commit by dan

--- lib/API/forms.api.dart
+++ lib/API/forms.api.dart
@@ -6,7 +6,7 @@ import 'package:dio/dio.dart';
 class FormsApi {
 //  static String apiUrl = 'https://6081-27-57-63-29.ngrok.io'  'http://127.0.0.1:8081'  'https://sppdemo.herokuapp.com';
 
-  static String apiUrl = 'https://72e7-2402-3a80-19a0-8d1a-18a-6497-fece-24c6.ngrok.io';
+  static String apiUrl = 'https://sjit-admin.herokuapp.com';
 
   final _dio = Dio(BaseOptions(baseUrl: apiUrl));
 
@@ -14,6 +14,7 @@ class FormsApi {
     final response = await _dio.post('/uploadForm', data: JSONdata);
     return "Success";
   }
+
 //Future<Response<dynamic>>
   dynamic downloadForm() async {
     var result = await _dio.post('/downloadForm');
