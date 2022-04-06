d7654d69b0f59a46eab68d02461cebb88369c72c
DANUSHRAJ <mldanushraj2001@gmail.com>
2022-04-06 09:00:06 +0530

    Notifications commit by dr

--- lib/Screens/Notifications.dart
+++ lib/Screens/Notifications.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-    
+import 'package:google_fonts/google_fonts.dart';
+
+import '../Home_screen.dart';
+
 class Notifications extends StatefulWidget {
   const Notifications({Key? key}) : super(key: key);
 
@@ -11,10 +14,42 @@ class _NotificationsState extends State<Notifications> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: const Text(''),
+        title: Text(
+          "NOTIFICATIONS",
+          textAlign: TextAlign.justify,
+          style: GoogleFonts.adventPro(
+              fontSize: 25,
+              color: Colors.orangeAccent,
+              fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        leading: _backButton(),
+        backgroundColor: Color.fromRGBO(30, 9, 23, 1),
+        elevation: 0,
       ),
       body: Container(),
+    );
+  }
+
+  Widget _backButton() {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => HomeScreen()));
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.only(left: 0, top: 10, bottom: 10),
+              child: const Icon(Icons.home_outlined, color: Colors.white),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
