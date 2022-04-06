16c330417453b5a07aaefd5c1eb8fbdfcffdeb94
DANUSHRAJ <mldanushraj2001@gmail.com>
2022-04-06 09:29:52 +0530

    Notifi push @9:29 by dr

--- lib/Screens/Notifications.dart
+++ lib/Screens/Notifications.dart
@@ -15,6 +15,7 @@ class _NotificationsState extends State<Notifications> {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
+      backgroundColor: Colors.amber,
       appBar: AppBar(
         title: Text(
           "NOTIFICATIONS",
@@ -29,7 +30,12 @@ class _NotificationsState extends State<Notifications> {
         backgroundColor: Color.fromRGBO(30, 9, 23, 1),
         elevation: 0,
       ),
-      body: Container(),
+      body: Container(
+          child: Column(
+        children: [
+          //  EmailInputFb1(),
+        ],
+      )),
     );
   }
 
@@ -51,5 +57,89 @@ class _NotificationsState extends State<Notifications> {
         ),
       ),
     );
+  }
+}
+
+gradient5() {
+  return LinearGradient(colors: [Colors.orange, Colors.yellow]);
+}
+
+class EmailInputFb1 extends StatefulWidget {
+  final TextEditingController inputController;
+  const EmailInputFb1({Key? key, required this.inputController})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const primaryColor = Color(0xff4338CA);
+    const secondaryColor = Color(0xff6D28D9);
+    const accentColor = Color(0xffffffff);
+    const backgroundColor = Color(0xffffffff);
+    const errorColor = Color(0xffEF4444);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Email",
+          style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.normal,
+              color: Colors.white.withOpacity(.9)),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Container(
+          height: 50,
+          decoration: BoxDecoration(boxShadow: [
+            BoxShadow(
+                offset: const Offset(12, 26),
+                blurRadius: 50,
+                spreadRadius: 0,
+                color: Colors.grey.withOpacity(.1)),
+          ]),
+          child: TextField(
+            controller: inputController,
+            onChanged: (value) {
+              //Do something wi
+            },
+            keyboardType: TextInputType.emailAddress,
+            style: const TextStyle(fontSize: 14, color: Colors.black),
+            decoration: InputDecoration(
+              // prefixIcon: Icon(Icons.email),
+              filled: true,
+              fillColor: accentColor,
+              hintText: 'Enter your email',
+              hintStyle: TextStyle(color: Colors.grey.withOpacity(.75)),
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
+              border: const OutlineInputBorder(
+                borderSide: BorderSide(color: primaryColor, width: 1.0),
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+              focusedBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: secondaryColor, width: 1.0),
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+              errorBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: errorColor, width: 1.0),
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+              enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: primaryColor, width: 1.0),
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
   }
 }
